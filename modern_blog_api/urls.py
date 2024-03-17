7569d6763f1240e60874ccba80ba5d21bd7b19ea
Theophilus Ackom <teamkweku@outlook.com>
2024-03-17 03:00:25 +0000

    setting up custom user model and swagger UI api documentation

--- modern_blog_api/urls.py
+++ modern_blog_api/urls.py
@@ -1,24 +1,37 @@
-"""modern_blog_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Modern Blog API",
+        default_version="v1",
+        description="API endpoints for the Modern Blog API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="modern_blog_api@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    path(
+        "swagger/<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     # setting admin to custom admin url path
     path(settings.ADMIN_URL, admin.site.urls),
 ]
+
+admin.site.site_header = "Modern Blog API"
+admin.site.site_title = "Modern Blog API Admin Portal"
+admin.site.index_title = "Welcome to the Modern Blog API Portal"
