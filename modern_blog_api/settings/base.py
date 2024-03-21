c85fc18e0efa7b1f6cb578ca9d42a72a059e8384
Theophilus Ackom <teamkweku@outlook.com>
2024-03-21 10:33:48 +0000

    Implementation of rate-limiting for Api

--- modern_blog_api/settings/base.py
+++ modern_blog_api/settings/base.py
@@ -211,7 +211,17 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    # enabling api rate-limiting
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/day",  # 100 requests per day for anonymous users
+        "user": "1000/day",  # 1000 requests per day for authenticated users
+    },
 }
+
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": (
